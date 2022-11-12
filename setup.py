e1b18c4ca02db0fb85e5cb72068e3873d1831d0b
Nathan Chowning <nathan@chowning.me>
2022-11-12 12:16:25 -0500

    Added basic setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="scrypy",
+    version="0.0.1",
+    description="Basic CLI scryfall client",
+    author="Nathan Chowning",
+    author_email="nathan@chowning.me",
+    scripts=["scry.py"]
+)
